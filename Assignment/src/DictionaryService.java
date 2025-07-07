899b5ec869493844f51ef9067ceded11ed31b6e2
TheAnhh7805 <anhkiet20085@gmail.com>
2025-07-07 21:56:42 +0700

    Create DictionaryService.java
    tạo interface cho dictionary với 2 method là addword và lookupWord

---
+++ Assignment/src/DictionaryService.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Acer
+ */
+public interface DictionaryService {
+    void addword(String word, String meaning);
+    String lookupWord(String word);
+}
