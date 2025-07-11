18f2d3b935d09b0014ff4935faf131c7f6a00b21
TheAnhh7805 <anhkiet20085@gmail.com>
2025-07-11 17:00:29 +0700

    Update Main.java

--- Assignment/src/Main.java
+++ Assignment/src/Main.java
@@ -1,3 +1,6 @@
+
+import java.io.IOException;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
@@ -12,8 +15,13 @@ public class Main {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // TODO code application logic here
+        DictionaryService basic = new BasicDictionary();
+        basic = new AddWord(basic);
+        basic = new HandleFile(basic);
+        basic = new UserInterface(basic);
+        ((UserInterface) basic).run();
     }
     
 }
